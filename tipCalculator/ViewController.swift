5339b7ffed79a6db0305f9974930ddd03fac52d6
Vinnie Chen <vinniechen@vinnies-mbp-2.fios-router.home>
2016-12-29 13:13:09 -0500

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Vinnie Chen on 12/29/16.
+//  Copyright © 2016 Vinnie Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
