23b41791b0209b0d9718d27855a50a26109d61a8
Vinnie Chen <vinniechen@vinnies-mbp-2.fios-router.home>
2016-12-29 19:37:43 -0500

    Calculates tips at 15% 20% and 25% when bill is entered into text field

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billTextField: UITextField!
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // hides keyboard on tap
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    // calculate tip once number entered in text field
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.15, 0.20, 0.25]
+        
+        
+        let bill = Double(billTextField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip) // %.2f two decimal places
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
